c710aa3d192c7b8ed1fdc048241713761dd9baa1
Siddharth Patel <sidharthpatel012@gmail.com>
2022-01-21 21:32:55 -0500

    Added backend functionality for the image upload through external node.js package called multer.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -11,8 +11,41 @@ const Post = require("../models/post");
  */
 const router = express.Router();
 
+// Multer is a node.js middleware for handling file uploads.
+const multer = require("multer");
+
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpg",
+  "image/jpg": "jpg",
+};
+
+// Multer configuration (where multer should put files)
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    // Security measure if file type does not match MIME_TYPE_MAP by throwing error
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mim type");
+    if (isValid) {
+      error = null;
+    }
+
+    // Callback is relevant to the server.js file which is why we must add backend/..
+    callback(error, "backend/images");
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.toLocaleLowerCase().split(" ").join("-");
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    callback(null, name + "-" + Date.now() + "." + ext);
+  },
+});
+
 /* Middle ware for post requests only. */
-router.post("", (request, response, next) => {
+/**
+ * multer(storage).single('image')
+ * Multer will try to extract a single file from the incoming request and will try to find it in the 'image' property in the request body.
+ */
+router.post("", multer(storage).single("image"), (request, response, next) => {
   /* One post */
   const post = new Post({
     title: request.body.title,
