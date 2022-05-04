85ec43c87169e725a50c62ee29d07b0e056819d7
Siddharth Patel <sidharthpatel@siddharths-air.lan>
2022-05-03 20:39:01 -0400

    Found a missing statement in post-list so fixed that.

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -65,6 +65,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   }
 
   onDelete(postId: string) {
+    this.isLoading = true;
     this.postsService.deletePost(postId).subscribe(() => {
       this.postsService.getPosts(this.postsPerPage, this.currentPage);
     });
