b5ba6ace01d5410f55b5df7bad035b7c189451ad
Siddharth Patel <sidharthpatel012@gmail.com>
2022-07-05 12:55:18 -0400

    Used the userId on the frontend for authorization.

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -75,6 +75,7 @@ export class PostsService {
       title: string;
       content: string;
       imagePath: string;
+      creator: string;
     }>('http://localhost:3000/api/posts/' + id);
   }
 
@@ -111,7 +112,7 @@ export class PostsService {
       postData.append('content', content);
       postData.append('image', image, title);
     } else {
-      postData = { id: id, title: title, content: content, imagePath: image };
+      postData = { id: id, title: title, content: content, imagePath: image, creator: null };
     }
     this.http
       .put('http://localhost:3000/api/posts/' + id, postData)
