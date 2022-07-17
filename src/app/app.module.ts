b791be413c34fdec3ad995d8249132395bac21b5
Siddharth Patel <sidharthpatel012@gmail.com>
2022-07-16 20:11:35 -0400

    Created an Error Interceptor that will display a basic error dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -21,6 +22,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error.interceptor';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +32,7 @@ import { ErrorInterceptor } from './error.interceptor';
     PostListComponent,
     LoginComponent,
     SignupComponent,
+    ErrorComponent,
   ],
   imports: [
     AppRoutingModule,
@@ -45,6 +48,7 @@ import { ErrorInterceptor } from './error.interceptor';
     MatExpansionModule,
     HttpClientModule,
     MatProgressSpinnerModule,
+    MatDialogModule,
     MatPaginatorModule,
   ],
   providers: [
@@ -52,5 +56,10 @@ import { ErrorInterceptor } from './error.interceptor';
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
+  /** Tells angular that this component is going to be used even though angular can't see it.
+   * We are are neither loading the below component through Selectors nor Routing.
+   * Hence, we need Angular to construct the component.
+  */
+  entryComponents: [ErrorComponent]
 })
 export class AppModule {}
--- src/app/error.interceptor.ts
+++ src/app/error.interceptor.ts
@@ -7,13 +7,20 @@ import {
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
+import { ErrorComponent } from './error/error.component';
+
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private dialog: MatDialog) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log(error);
-        alert(error.error.error.message);
+        this.dialog.open(ErrorComponent);
         return throwError(error);
       })
     );
