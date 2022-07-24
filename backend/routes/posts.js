3c247435f68f60980a455ad5e5f31e88d3079448
Siddharth Patel <sidharthpatel012@gmail.com>
2022-07-24 02:04:40 -0400

    Created a helper class for the backend user and post routes using controllers.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -4,8 +4,6 @@
 
 const express = require("express");
 
-const Post = require("../models/post");
-
 const checkAuth = require("../middleware/check-auth");
 
 /**
@@ -15,6 +13,8 @@ const router = express.Router();
 
 // Multer is a node.js middleware for handling file uploads.
 const multer = require("multer");
+
+const PostController = require("../controllers/posts");
 
 const MIME_TYPE_MAP = {
   "image/png": "png",
@@ -53,37 +53,7 @@ router.post(
    * Express will take care of automatically passing in arguments to the variable when it enters the middleware. */
   checkAuth,
   multer({ storage: storage }).single("image"),
-  (request, response, next) => {
-    /**
-     * URL to the image file.
-     * request.protocol: Returns the type of protocol you are using ('http/https')
-     */
-    const url = request.protocol + "://" + request.get("host");
-
-    /* One post */
-    const post = new Post({
-      title: request.body.title,
-      content: request.body.content,
-      /* request.file: Property provided by Multer to extract file's name. */
-      imagePath: url + "/images/" + request.file.filename,
-      creator: request.userData.userId,
-    });
-    /** Default Mongoose method */
-    post
-      .save()
-      .then((createdPost) => {
-        response.status(201).json({
-          message: "Post added successfully!",
-          post: {
-            ...createdPost,
-            id: createdPost._id,
-          },
-        });
-      })
-      .catch((error) => {
-        response.status(500).json({ message: "Creating a post failed!" });
-      });
-  }
+  PostController.createPost
 );
 
 /**
@@ -95,105 +65,20 @@ router.put(
   "/:id",
   checkAuth,
   multer({ storage: storage }).single("image"),
-  (req, res, next) => {
-    //Default image path naming convention
-    let imagePath = req.body.imagePath;
-    if (req.file) {
-      const url = req.protocol + "://" + req.get("host");
-      imagePath = url + "/images/" + req.file.filename;
-    }
-    const post = new Post({
-      _id: req.body.id,
-      title: req.body.title,
-      content: req.body.content,
-      imagePath: imagePath,
-      creator: req.userData.userId,
-    });
-    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
-      .then((result) => {
-        if (result.nModified > 0) {
-          res.status(200).json({ message: "Update successful!" });
-        } else {
-          res.status(401).json({ message: "Not autorized!" });
-        }
-      })
-      .catch((error) => {
-        res.status(500).json({ message: "Couldn't update post" });
-      });
-  }
+  PostController.updatePost
 );
 
 /**
  * Only the /api/posts requests will be handled by our backend.
  * /api is to ensure we are dealing with ONLY the REST api requests.
  */
-router.get("", (request, response, next) => {
-  //Generating API that works with the given const/ query params below.
-  // + converts the query string into integer.
-  const pageSize = +request.query.pagesize;
-  const currentPage = +request.query.page;
-  const postQuery = Post.find();
+router.get("", PostController.getPosts);
 
-  let fetchedPosts;
+/**
+ * Fetching a single post given an Id.
+ */
+router.get("/:id", PostController.getPost);
 
-  if (pageSize && currentPage) {
-    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
-  }
-
-  /**
-   * First, I am fetching all the posts,
-   * Then I am issuing another query to get the number of posts.
-   */
-  postQuery
-    .then((documents) => {
-      fetchedPosts = documents;
-      return Post.count();
-    })
-    .then((count) => {
-      response.status(200).json({
-        message: "Posts fetched successfully",
-        posts: fetchedPosts,
-        maxPosts: count,
-      });
-    })
-    .catch((error) => {
-      response.status(500).json({
-        message: "Fetching posts failed!",
-      });
-    });
-});
-
-router.get("/:id", (req, res, next) => {
-  Post.findById(req.params.id)
-    .then((post) => {
-      // Checks if the post exists
-      if (post) {
-        res.status(200).json(post);
-      } else {
-        res.status(404).json({ message: "Post Not Found!" });
-      }
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: "Fetching post failed!",
-      });
-    });
-});
-
-router.delete("/:id", checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
-    .then((result) => {
-      if (result.n > 0) {
-        res.status(200).json({ message: "Deletion successful!" });
-      } else {
-        res.status(401).json({ message: "Not autorized!" });
-      }
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: "Fetching post failed!",
-      });
-    });
-});
+router.delete("/:id", checkAuth, PostController.deletePost);
 
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,75 +2,19 @@
  * User.js file is a backend file to authenticate user email and password fetched from frontend and add it to the database for future retrieval of data.
  */
 const express = require("express");
-const User = require("../models/user");
 const router = express.Router();
 
-const bcrpyt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const UserController = require("../controllers/user");
 
-router.post("/signup", (req, res, next) => {
-  bcrpyt.hash(req.body.password, 10).then((hash) => {
-    const user = new User({
-      email: req.body.email,
-      /**
-       * This is SUPER BAD!!! If anyone gets an access to our database, they basically have our password since it is saved as a raw text.
-       * It is always effective to save passwords encrpyted.
-       */
-      // password: req.body.password,
-      password: hash,
-    });
-    user
-      .save()
-      .then((result) => {
-        res.status(201).json({
-          message: "User created",
-          result: result,
-        });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          message: "Invalid authentication credentials!",
-        });
-      });
-  });
-});
+/**
+ * Note we are not using createUser() since it is a function to be executed.
+ * We are using createUser as a reference so the pherentheses are unnecessary.
+ */
+router.post("/signup", UserController.createUser);
 
 /**
  * Generate token on this route.
  */
-router.post("/login", (req, res, next) => {
-  let fetchedUser;
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({
-          message: "Auth Failed",
-        });
-      }
-      fetchedUser = user;
-      return bcrpyt.compare(req.body.password, user.password);
-    })
-    .then((result) => {
-      if (!result) {
-        return res.status(401).json({
-          message: "Auth Failed",
-        });
-      }
-      /* Creates a new token */
-      const token = jwt.sign(
-        { email: fetchedUser.email, userId: fetchedUser._id },
-        "secret_this_should_be_longer",
-        { expiresIn: "1h" }
-      );
-      res
-        .status(200)
-        .json({ token: token, expiresIn: 3600, userId: fetchedUser._id });
-    })
-    .catch((err) => {
-      return res.status(401).json({
-        message: "Invalid authentication credentials!",
-      });
-    });
-});
+router.post("/login", UserController.loginUser);
 
 module.exports = router;
